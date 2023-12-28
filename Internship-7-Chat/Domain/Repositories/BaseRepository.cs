ebe202859e81faa481bdb42179190dacaddc0092
SimeJadrijev <sime.jadrijev@gmail.com>
2023-12-28 01:01:46 +0100

    Added the ResponseResultType.cs enum and started working on Repositories folder

---
+++ Internship-7-Chat/Domain/Repositories/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using Data.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly ChatAppDbContext DbContext;
+
+        protected BaseRepository(ChatAppDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        protected ResponseResultType SaveChanges()
+        {
+            var hasChanges = DbContext.SaveChanges() > 0;
+            if (hasChanges)
+                return ResponseResultType.Success;
+            return ResponseResultType.NoChanges;
+        }
+    }
+}
---
+++ Internship-7-Chat/Domain/Repositories/GroupRepository.cs
@@ -0,0 +1,48 @@
+﻿using Data.Entities;
+using Data.Entities.Models;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public class GroupRepository : BaseRepository
+    {
+        public GroupRepository(ChatAppDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public ResponseResultType Add(Group group)
+        {
+            DbContext.Groups.Add(group);
+
+            return SaveChanges();
+        }
+
+        public ResponseResultType Update(Group group, int id)
+        {
+            var groupForUpdate = DbContext.Groups.Find(id);
+            if (groupForUpdate is null)
+            {
+                return ResponseResultType.NotFound;
+            }
+
+            groupForUpdate.User = group.User;
+            groupForUpdate.GroupName = group.GroupName;
+            groupForUpdate.GroupMessages = group.GroupMessages;
+            groupForUpdate.UserCreatorID = group.UserCreatorID;
+            groupForUpdate.GroupID = group.GroupID;
+
+            return SaveChanges();
+        }
+
+        public Group? GetById(int id) => DbContext.Groups.FirstOrDefault(c => c.GroupID == id);
+        public ICollection<Group> GetAll() => DbContext.Groups.ToList();
+
+
+    }
+}
