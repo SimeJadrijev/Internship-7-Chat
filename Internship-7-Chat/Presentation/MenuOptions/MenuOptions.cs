b6f95cf26e789f530b1b8437074f2c0f07681af3
SimeJadrijev <sime.jadrijev@gmail.com>
2024-01-02 01:28:51 +0100

    Added Lists of menu options, but without any actions so far

--- Internship-7-Chat/Presentation/MenuOptions/MenuOptions.cs
+++ Internship-7-Chat/Presentation/MenuOptions/MenuOptions.cs
@@ -16,7 +16,50 @@ namespace Presentation.MenuOptions
             User = _user;
         }
 
+        public List<(string, Action)> GroupChatOptions()
+        {
+            var options = new List<(string, Action)>()
+            {
+                ("Kreiranje novog grupnog chata", () => Console.WriteLine("Work in progress...")),
+                ("Ulazak u grupni chat", () => Console.WriteLine("Work in progress...")),
+                ("Ispis svih grupnih", () => Console.WriteLine("Work in progress...")),
+                ("Povratak na prethodni Menu", () => Console.WriteLine("Work in progress..."))
 
+            };
 
+            return options;
+        }
+
+        public List<(string, Action)> PrivateMessageOptions()
+        {
+            var options = new List<(string, Action)>()
+            {
+                ("Nova poruka", () => Console.WriteLine("Work in progress...")),
+                ("Ispis svih privatnih poruka", () => Console.WriteLine("Work in progress...")),
+                ("Povratak na prethodni Menu", () => Console.WriteLine("Work in progress..."))
+
+            };
+
+            return options;
+        }
+
+        public List<(string, Action)> UserManagment()
+        {
+            var options = new List<(string, Action)>() { ("UserManagament", () => Console.WriteLine("Work in progress...")) };
+            return options;
+        }
+
+        public List<(string, Action)> UserSettings()
+        {
+            var options = new List<(string, Action)>()
+            {
+                ("Promjena email adrese", () => Console.WriteLine("Work in progress...")),
+                ("Promjena lozinke", () => Console.WriteLine("Work in progress...")),
+                ("Povratak na prethodni Menu", () => Console.WriteLine("Work in progress..."))
+
+            };
+
+            return options;
+        }
     }
 }
